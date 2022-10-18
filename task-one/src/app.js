4500a638c8b5cd14b5ad63cd99e58f57373cd38f
mac <mac@chidinmas-MacBook-Air.local>
2022-10-18 23:13:39 +0100

    first commit

--- task-one/src/app.js
+++ task-one/src/app.js
@@ -3,7 +3,18 @@ import FileTree from './fileTree';
 export function createFileTree(input) {
   const fileTree = new FileTree();
 
-  for (const inputNode of input) {
+  const newArr = [];
+  for (let i = 0; i < input.length; i++) {
+    if (!input[i].parentId) {
+      newArr.push(input[i]);
+      input.splice(input[i], 1);
+    }
+  }
+const inputData = input.sort((a, b) => a.id - b.id)
+
+  const result = [...newArr, ...inputData];
+
+  for (const inputNode of result) {
     const parentNode = inputNode.parentId
       ? fileTree.findNodeById(inputNode.parentId)
       : null;
